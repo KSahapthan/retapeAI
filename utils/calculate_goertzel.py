6209565a2094b0628401982a0b4dc82e7fb0a460
KSahapthan <sahapthan2004@gmail.com>
2025-12-27 22:38:39 +0530

    remove pycache

--- utils/calculate_goertzel.py
+++ utils/calculate_goertzel.py
@@ -12,6 +12,7 @@ def calculate_goertzel(chunk: np.ndarray, target_freq=1000, sample_rate=8000) ->
     omega = (2.0 * np.pi * k) / N
     coeff = 2.0 * np.cos(omega)
 
+    # Goertzel algorithm
     s0, s1, s2 = 0.0, 0.0, 0.0
     for sample in chunk:
         s0 = float(sample) + coeff * s1 - s2
